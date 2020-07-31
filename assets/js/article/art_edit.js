866590f17670d1d0ce5cd894b9cd91d89889269b
yujie <1049250128@qq.com>
2020-07-31 19:39:43 +0800

    完成文章管理相关功能的开发

---
+++ assets/js/article/art_edit.js
@@ -0,0 +1,154 @@
+$(function () {
+  var form = layui.form;
+
+  // 获取到地址栏的Id参数
+  // var $Id = getUrlParams("Id");
+  // 从浏览器的地址栏中获取查询参数
+  // function getUrlParams(name) {
+  //   var paramsAry = location.search.substr(1).split("&");
+  //   // 循环数据
+  //   for (var i = 0; i < paramsAry.length; i++) {
+  //     var tmp = paramsAry[i].split("=");
+  //     if (tmp[0] == name) {
+  //       return tmp[1];
+  //     }
+  //   }
+  //   return -1;
+  // }
+
+  // 获取地址栏参数
+  var $Id = new URLSearchParams(location.search).get("Id");
+  console.log($Id);
+
+  // 1. 初始化图片裁剪器
+  var $image = $("#image");
+  // 2. 裁剪选项
+  var options = {
+    aspectRatio: 400 / 280,
+    preview: ".img-preview",
+  };
+  // 初始化富文本编辑器
+  initEditor();
+
+  //   调用Ajax查询文章旧数据
+  $.ajax({
+    type: "get",
+    url: "/my/article/" + $Id,
+    success: function (res) {
+      console.log(res);
+
+      //  把旧数据进行表单填充
+      form.val("art_edit", res.data);
+      // 把当前所属的分类传进去，进行默认选中的判断
+      initCate(res.data.cate_id);
+
+      // 3. 初始化裁剪区域 把当前封面地址进行替换
+      $image
+        .cropper("destroy") // 销毁旧的裁剪区域
+        .attr("src", "http://ajax.frontend.itheima.net" + res.data.cover_img) // 重新设置图片路径
+        .cropper(options); // 重新初始化裁剪区域
+    },
+  });
+
+  // 定义加载文章分类的方法
+  function initCate(currentCateId = "") {
+    $.ajax({
+      method: "GET",
+      url: "/my/article/cates",
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("初始化文章分类失败！");
+        }
+        res.currentCateId = currentCateId;
+        // 调用模板引擎，渲染分类的下拉菜单
+        var htmlStr = template("tpl-cate", res);
+        $("[name=cate_id]").html(htmlStr);
+        // 一定要记得调用 form.render() 方法
+        form.render();
+      },
+    });
+  }
+
+  // 为选择封面的按钮，绑定点击事件处理函数
+  $("#btnChooseImage").on("click", function () {
+    $("#coverFile").click();
+  });
+
+  // 监听 coverFile 的 change 事件，获取用户选择的文件列表
+  $("#coverFile").on("change", function (e) {
+    // 获取到文件的列表数组
+    var files = e.target.files;
+    // 判断用户是否选择了文件
+    if (files.length === 0) {
+      return;
+    }
+    // 根据文件，创建对应的 URL 地址
+    var newImgURL = URL.createObjectURL(files[0]);
+    // 为裁剪区域重新设置图片
+    $image
+      .cropper("destroy") // 销毁旧的裁剪区域
+      .attr("src", newImgURL) // 重新设置图片路径
+      .cropper(options); // 重新初始化裁剪区域
+  });
+
+  // 定义文章的发布状态
+  var art_state = "已发布";
+
+  // 为存为草稿按钮，绑定点击事件处理函数
+  $("#btnSave2").on("click", function () {
+    art_state = "草稿";
+  });
+
+  // 为表单绑定 submit 提交事件
+  $("#form-edit").on("submit", function (e) {
+    // 1. 阻止表单的默认提交行为
+    e.preventDefault();
+    // 2. 基于 form 表单，快速创建一个 FormData 对象
+    var fd = new FormData($(this)[0]);
+    console.log($("textarea").val());
+
+    fd.forEach(function (k, v) {
+      console.log(k + "===" + v);
+    });
+    // 把当前文章的Id加入到fd中
+    fd.append("Id", $Id);
+    // 3. 将文章的发布状态，存到 fd 中
+    fd.append("state", art_state);
+    // 4. 将封面裁剪过后的图片，输出为一个文件对象
+    $image
+      .cropper("getCroppedCanvas", {
+        // 创建一个 Canvas 画布
+        width: 400,
+        height: 280,
+      })
+      .toBlob(function (blob) {
+        // 将 Canvas 画布上的内容，转化为文件对象
+        // 得到文件对象后，进行后续的操作
+        // 5. 将文件对象，存储到 fd 中
+        fd.append("cover_img", blob);
+        // 6. 发起 ajax 数据请求
+        editArticle(fd);
+      });
+  });
+
+  // 定义一个发布文章的方法
+  function editArticle(fd) {
+    $.ajax({
+      method: "POST",
+      url: "/my/article/edit",
+      data: fd,
+      // 注意：如果向服务器提交的是 FormData 格式的数据，
+      // 必须添加以下两个配置项
+      contentType: false,
+      processData: false,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("修改文章失败！");
+        }
+        layer.msg("修改文章成功！");
+        // 发布文章成功后，跳转到文章列表页面
+        location.href = "/article/art_list.html";
+      },
+    });
+  }
+});
